a584b2e83f6a229ab94d95b848340131fdbe55c8
Zay <contact@zay-dev.com>
2022-02-12 15:14:42 +0800

    implemented the entities interface in the EF core project

---
+++ Facilis.Core/Facilis.Core.EntityFrameworkCore/EntitiesWithoutStatus.cs
@@ -0,0 +1,108 @@
+﻿using Facilis.Core.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Facilis.Core.EntityFrameworkCore
+{
+    public class EntitiesWithoutStatus<T> : IEntities<T>
+        where T : class, IEntityWithId
+    {
+        private DbContext context { get; }
+
+        protected virtual DbSet<T> Set => this.context.Set<T>();
+
+        public virtual IQueryable<T> Entities => this.Set;
+
+        #region Constructor(s)
+
+        public EntitiesWithoutStatus(DbContext context)
+        {
+            this.context = context;
+        }
+
+        #endregion Constructor(s)
+
+        public virtual T Add(T entity)
+        {
+            this.Set.Add(entity);
+            this.context.SaveChanges();
+            return entity;
+        }
+
+        public virtual T[] Add(T[] entities)
+        {
+            this.Set.AddRange(entities);
+            this.context.SaveChanges();
+            return entities;
+        }
+
+        public virtual T[] Add(IEnumerable<T> entities)
+        {
+            return this.Add(entities.ToArray());
+        }
+
+        public virtual void AddNoSave(params T[] entities)
+        {
+            this.Set.AddRange(entities);
+        }
+
+        public virtual void AddNoSave(IEnumerable<T> entities)
+        {
+            this.AddNoSave(entities.ToArray());
+        }
+
+        public virtual T FindById(string id)
+        {
+            return this.Entities
+                .FirstOrDefault(entity => entity.Id == id);
+        }
+
+        public virtual T[] FindByIds(params string[] ids)
+        {
+            return this.Entities.Where(entity => ids.Contains(entity.Id)).ToArray();
+        }
+
+        public virtual T[] FindByIds(IEnumerable<string> ids)
+        {
+            return this.FindByIds(ids.ToArray());
+        }
+
+        public virtual void Save()
+        {
+            this.context.SaveChanges();
+        }
+
+        public virtual T Update(T entity)
+        {
+            this.Set.Update(entity);
+            this.context.SaveChanges();
+
+            return entity;
+        }
+
+        public virtual T[] Update(T[] entities)
+        {
+            this.Set.UpdateRange(entities);
+            this.context.SaveChanges();
+
+            return entities;
+        }
+
+        public virtual T[] Update(IEnumerable<T> entities)
+        {
+            return this.Update(entities.ToArray());
+        }
+
+        public virtual void UpdateNoSave(params T[] entities)
+        {
+            this.Set.UpdateRange(entities);
+        }
+
+        public virtual void UpdateNoSave(IEnumerable<T> entities)
+        {
+            this.UpdateNoSave(entities.ToArray());
+        }
+    }
+}
