927aa797991b232993a6ccca8956045cabdb2e58
Zay <contact@zay-dev.com>
2022-02-19 12:16:38 +0800

    created the TODO app dotnet 6 web project

---
+++ Facilis.Core/Facilis.ToDoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Facilis.ToDoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
