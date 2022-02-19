97f25b1ee3d428327cf8b7fc47ce116a0bdd1692
Zay <contact@zay-dev.com>
2022-02-19 14:09:48 +0800

    configured the basic db option for Sqlite

---
+++ Facilis.Core/Facilis.ToDoApp/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Facilis.Core.Abstractions;
+using Facilis.Core.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Facilis.ToDoApp
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+        #region Constructor(s)
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        #endregion Constructor(s)
+    }
+
+    public class ToDoItem : IEntityWithId, IEntityWithStatus
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public StatusTypes Status { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
--- Facilis.Core/Facilis.ToDoApp/Program.cs
+++ Facilis.Core/Facilis.ToDoApp/Program.cs
@@ -1,27 +1,47 @@
-var builder = WebApplication.CreateBuilder(args);
+using Facilis.ToDoApp;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+static void ConfigureService(WebApplicationBuilder builder)
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    builder.Services.AddControllersWithViews();
+    builder.Services
+        .AddDbContext<AppDbContext>(option =>
+            option.UseSqlite("Data Source=./local.db;")
+        )
+        .AddDbContext<DbContext, AppDbContext>();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+static void Configure(WebApplication app)
+{
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
-app.UseRouting();
+    using var scope = app.Services.CreateScope();
+    scope.ServiceProvider
+        .GetService<DbContext>()
+        .Database
+        .EnsureCreated();
 
-app.UseAuthorization();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.UseRouting();
 
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+}
+
+var builder = WebApplication.CreateBuilder(args);
+ConfigureService(builder);
+
+using var app = builder.Build();
+Configure(app);
 app.Run();
