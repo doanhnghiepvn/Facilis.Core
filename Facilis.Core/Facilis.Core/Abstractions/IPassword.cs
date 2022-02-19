4b489affc6376ac813e2473a346fc910c6067a34
Zay <contact@zay-dev.com>
2022-02-20 00:40:34 +0800

    created the password interfaces

---
+++ Facilis.Core/Facilis.Core/Abstractions/IPassword.cs
@@ -0,0 +1,10 @@
+﻿namespace Facilis.Core.Abstractions
+{
+    public interface IPassword
+    {
+        string HashingMethod { get; }
+        string HashedPassword { get; }
+        string PasswordSalt { get; }
+        int PasswordIterated { get; }
+    }
+}
