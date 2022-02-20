86efef5e69cac39aace87b0086f753f02e182df5
Zay <contact@zay-dev.com>
2022-02-20 11:56:48 +0800

    created the entity with scope and key value pair interfaces

---
+++ Facilis.Core/Facilis.Core/Abstractions/IEntityWithKeyValuePair.cs
@@ -0,0 +1,12 @@
+﻿namespace Facilis.Core.Abstractions
+{
+    public interface IEntityWithKeyValuePair<T>
+    {
+        string Key { get; }
+        T Value { get; }
+    }
+
+    public interface IEntityWithKeyValuePair : IEntityWithKeyValuePair<string>
+    {
+    }
+}
---
+++ Facilis.Core/Facilis.Core/Abstractions/IEntityWithScope.cs
@@ -0,0 +1,8 @@
+﻿namespace Facilis.Core.Abstractions
+{
+    public interface IEntityWithScope
+    {
+        string Scope { get; }
+        string ScopedId { get; }
+    }
+}
