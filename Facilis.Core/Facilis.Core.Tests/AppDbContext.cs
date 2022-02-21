d3b203841852b6c6cd6b4c52a95a36b54748fe29
Zay <contact@zay-dev.com>
2022-02-22 03:10:05 +0800

    created the user model in the test project

--- Facilis.Core/Facilis.Core.Tests/AppDbContext.cs
+++ Facilis.Core/Facilis.Core.Tests/AppDbContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Facilis.Core.Tests.Models;
+using Facilis.ExtendedEntities.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Facilis.Core.Tests
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<ExtendedAttribute> ExtendedAttributes { get; set; }
+
         #region Constructor(s)
 
         public AppDbContext(DbContextOptions options) : base(options)
---
+++ Facilis.Core/Facilis.Core.Tests/Models/User.cs
@@ -0,0 +1,54 @@
+﻿using Facilis.Core.Abstractions;
+using Facilis.Core.Enums;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Text.Json;
+
+namespace Facilis.Core.Tests.Models
+{
+    public class User : IEntityWithId, IEntityWithStatus, IEntityWithProfile
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public StatusTypes Status { get; set; }
+
+        [NotMapped]
+        public object Profile { get; set; }
+
+        public string SerializedProfile { get; set; }
+
+        #region Constructor(s)
+
+        public User()
+        {
+            this.Profile = this.GetProfile<UserProfile>();
+        }
+
+        #endregion Constructor(s)
+
+        public T GetProfile<T>()
+        {
+            return this.SerializedProfile == null ? default :
+                JsonSerializer.Deserialize<T>(this.SerializedProfile);
+        }
+
+        public void SetProfile(object profile)
+        {
+            this.Profile = profile;
+            this.SerializedProfile = JsonSerializer.Serialize(profile);
+        }
+    }
+
+    public class UserProfile
+    {
+        public string FirstName { get; set; } = "Tony";
+        public string LastName { get; set; } = "Stark";
+        public string Phone { get; set; }
+        public int Age { get; set; } = 48;
+        public string[] AddressLines { get; set; } = { "890 Fifth Avenue", "Manhattan", "New York City" };
+        public decimal HeightInCm { get; set; } = 185.42m;
+        public DateTime LastSignInAtUtc { get; set; } = DateTime.UtcNow;
+        public FileInfo Avatar { get; set; }
+        public UserProfile Copy { get; set; }
+    }
+}
