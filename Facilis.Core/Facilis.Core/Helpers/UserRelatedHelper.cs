5bf78f59e7899981f8a3645e029ef32012d95389
Zay <contact@zay-dev.com>
2022-03-11 17:15:25 +0800

    created the user related helper class

---
+++ Facilis.Core/Facilis.Core/Helpers/UserRelatedHelper.cs
@@ -0,0 +1,21 @@
+﻿using Facilis.Core.Abstractions;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Facilis.Core.Helpers
+{
+    public static class UserRelatedHelper
+    {
+        public static IQueryable<T> WhereEnabledByUserId<T>(
+            this IEntities<T> entities,
+            string userId,
+            Expression<Func<T, bool>> expression = null
+        ) where T : IEntityWithId, IEntityWithStatus, IUserRelatedEntity
+        {
+            return entities
+                .WhereEnabled(expression)
+                .Where(entity => entity.UserId == userId);
+        }
+    }
+}
